c4430c6a46c2f4bf1595a3c4fba2cbaa56d3607f
norikoda <kodaka997531@gmail.com>
2022-03-27 14:13:49 +0900

    usersテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   devise_for :installs
+=======
+  devise_for :users
+>>>>>>> Stashed changes
   get 'messages/index'
   root to: "messages#index"
 end
